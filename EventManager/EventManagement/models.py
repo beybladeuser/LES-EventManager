57071acb72e3114852ecac070fcacbd02ae0056b
caparritos <pdavid1999@hotmail.com>
2021-04-28 16:06:02 +0100

    cancelregistrations

--- EventManager/EventManagement/models.py
+++ EventManager/EventManagement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Resgistration(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    eventid_event = models.ForeignKey('Event', models.DO_NOTHING, db_column='EventID_Event')  # Field name made lowercase.
+    date = models.IntegerField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
+    waspresent = models.IntegerField(db_column='WasPresent', blank=True, null=True)  # Field name made lowercase.
+    participantuserid = models.ForeignKey(Participante, models.DO_NOTHING, db_column='ParticipantUserID')  # Field name made lowercase.
+
+def cancelregistrations(self) :
+        return Answer.objects.filter(Registrationid_answer=id_registrations , participantuserid_registrations=id.authUser).delete()
+
+
+
+    class Meta:
+        
+        db_table = 'resgistration'
--- EventManager/Models/models.py
+++ EventManager/Models/models.py
@@ -11,60 +11,7 @@ from AssetManagement.models import *
 from Sessions.models import *
 
 
-class AssetEvent(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    assetid_asset = models.ForeignKey('AssetManagement.Asset', models.DO_NOTHING, db_column='AssetID_Asset')  # Field name made lowercase.
-    eventid_event = models.ForeignKey('Event', models.DO_NOTHING, db_column='EventID_Event')  # Field name made lowercase.
 
-    class Meta:
-        
-        db_table = 'asset_event'
-
-
-class AssetLogistics(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventid_event = models.ForeignKey('Event', models.DO_NOTHING, db_column='EventID_Event')  # Field name made lowercase.
-    servicetypeid_servicetype = models.ForeignKey('AssetManagement.Servicetype', models.DO_NOTHING, db_column='ServiceTypeID_ServiceType', blank=True, null=True)  # Field name made lowercase.
-    equipmenttypeid_equipmenttype = models.ForeignKey('AssetManagement.Equipmenttype', models.DO_NOTHING, db_column='EquipmentTypeID_EquipmentType', blank=True, null=True)  # Field name made lowercase.
-    seats = models.IntegerField(db_column='Seats', blank=True, null=True)  # Field name made lowercase.
-    seatsforreducedmobility = models.IntegerField(db_column='SeatsForReducedMobility', blank=True, null=True)  # Field name made lowercase.
-    quantity = models.IntegerField(db_column='Quantity')  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'asset_logistics'
-
-class Event(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventtypeid = models.ForeignKey('Eventtype', models.DO_NOTHING, db_column='EventTypeID')  # Field name made lowercase.
-    formresgistrationid = models.ForeignKey('FormManagement.Form', models.DO_NOTHING, db_column='FormResgistrationID', related_name='Event2registerForm')  # Field name made lowercase.
-    formfeedbackid = models.ForeignKey('FormManagement.Form', models.DO_NOTHING, db_column='FormFeedBackID', related_name='Event2feedbackForm')  # Field name made lowercase.
-    campusid = models.ForeignKey(Campus, models.DO_NOTHING, db_column='CampusID')  # Field name made lowercase.
-    wasvalidated = models.TextField(db_column='wasValidated')  # Field name made lowercase. This field type is a guess.
-    proponentid = models.ForeignKey('Sessions.Utilizador', models.DO_NOTHING, db_column='ProponentID')  # Field name made lowercase.
-    eventname = models.CharField(db_column='eventName', max_length=255)  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'event'
-
-
-class Eventtype(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    typename = models.CharField(db_column='TypeName', max_length=255)  # Field name made lowercase.
-
-    @staticmethod
-    def makeOptions() :
-        if "eventtype" in connection.introspection.table_names() :
-            eventTypes = Eventtype.objects.all()
-            options=([(eventType.id, eventType.typename) for eventType in eventTypes])
-            return options
-        else:
-            return (("1", "No Database created"),)
-
-    class Meta:
-        
-        db_table = 'eventtype'
 
 
 class Informacaomensagem(models.Model):
@@ -137,18 +84,6 @@ class Notificacao(models.Model):
     class Meta:
         
         db_table = 'notificacao'
-
-
-class Resgistration(models.Model):
-    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    eventid_event = models.ForeignKey('Event', models.DO_NOTHING, db_column='EventID_Event')  # Field name made lowercase.
-    date = models.IntegerField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
-    waspresent = models.IntegerField(db_column='WasPresent', blank=True, null=True)  # Field name made lowercase.
-    participantuserid = models.ForeignKey(Participante, models.DO_NOTHING, db_column='ParticipantUserID')  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'resgistration'
 
 
 
