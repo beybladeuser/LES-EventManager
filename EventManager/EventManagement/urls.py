26c6dca5596be5cb6bb28943ebfa4bdeb19efc3f
beybladeuser <rubencruz0312@gmail.com>
2021-04-28 16:53:03 +0100

    Update urls.py

--- EventManager/EventManagement/urls.py
+++ EventManager/EventManagement/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cancelregistrations, name='cancelregistrations'),
+    path('', views.cancelregistration, name='cancelregistration'),
 ]
