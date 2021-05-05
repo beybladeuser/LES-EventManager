95f44b85c9438e8d372d0e68248ff2fe92eb9997
caparritos <pdavid1999@hotmail.com>
2021-05-05 14:22:22 +0100

    addregistration

--- EventManager/EventManagement/views.py
+++ EventManager/EventManagement/views.py
@@ -27,7 +27,29 @@ def consultar_participantes(request,eventid_event) :
         return redirect("index")
        
     
+def addregistration(request, formID = 1):
+    regis = Resgistration()
+    regis.eventid_event = Event.objects.get(id=1)
+    regis.participantuserid = request.user
+    if request.method == 'POST':
+        form = EventManagerForm(request.POST, eventManagerFormID=formID, associatedRegistration=regis, associatedEvent=None)
+        if form.is_valid():
+            regis.save()
+            answeredForm = form.save()
+            request.session['form_return_redirect'] = "/forms/listformsfromtype/" + str(answeredForm.formtypeid_formtype.id)
+            return redirect("checkFormLayout", answeredForm.id)
+    
+    else:
+        form = EventManagerForm(eventManagerFormID=formID, associatedRegistration=regis, associatedEvent=None)
+    
+    
 
+
+    template = loader.get_template('template_test_form.html')
+    context = {
+        'form' : form,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 # def checkboxvalues(request)
