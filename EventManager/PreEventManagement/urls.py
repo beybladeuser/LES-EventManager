2c609c888b0340ae84f7a0c172156e765b9e7fb8
caparritos <pdavid1999@hotmail.com>
2021-05-12 14:23:19 +0100

    4 commit

--- EventManager/PreEventManagement/urls.py
+++ EventManager/PreEventManagement/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.preEventHome, name='preEventHome'),
-    path('', views.eventList, name='eventList')
+    path('', views.index, name='index'),
+    path('list', views.list, name='list'),
+    path('create', views.create, name='create'),
+    path('edit/<int:id>', views.list, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('validate/<int:id>', views.validate, name='validate')
 ]
