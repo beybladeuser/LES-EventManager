456e1c39ccf7febe31ee98ae7118a63f0e753933
beybladeuser <rubencruz0312@gmail.com>
2021-05-15 23:44:19 +0100

    update to ui

--- EventManager/FormManagement/views.py
+++ EventManager/FormManagement/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 from FormManagement.models import Form, Formtype, QuestionsForm, Questions, Multipleoptions, Questiontype, Answer
 from Models.models import *
 
+from django_tables2 import SingleTableMixin
+from django_filters.views import FilterView
+
 from .forms import *
+from .tables import *
+from .filters import *
+
+from utilizadores.views import user_check
 
 def formsHome(request) :
 
@@ -132,6 +139,29 @@ def listFormsFromType(request, formTypeID = None) :
         'errorMessage' : errorMessage,
     }
     return HttpResponse(template.render(context, request))
+
+
+class listFormsFromType_new(SingleTableMixin, FilterView) :
+    table_class = formsTable
+    template_name = 'template_list_forms.html'
+    filterset_class = formsFilter
+    table_pagination = {
+        'per_page': 10
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        user_check_var = user_check(request=request, user_profile=[Administrador, Proponente])
+        if not user_check_var.get('exists'):
+            return user_check_var.get('render')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(SingleTableMixin, self).get_context_data(**kwargs)
+        table = self.get_table(**self.get_table_kwargs())
+        table.request = self.request
+        table.fixed = True
+        context[self.get_context_table_name(table)] = table
+        return context
 
 
 def createForm(request, formTypeID=None, formID=None) :
