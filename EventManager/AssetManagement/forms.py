274eb15d75a5fe1fa69ba7573d5e0ecaf75a795b
beybladeuser <rubencruz0312@gmail.com>
2021-05-19 19:26:42 +0100

    fixed some erros

--- EventManager/AssetManagement/forms.py
+++ EventManager/AssetManagement/forms.py
@@ -1,45 +1,45 @@
-from django import forms
-from .models import Asset
-import datetime
-from pip._vendor.urllib3 import request
-from FormManagement.models import Form
-
-
-class InsertAssetForm(forms.Form):
-	user = None
-	assetName = forms.CharField(label='Asset Name', max_length=255, required=False)
-	assetQuantity = forms.IntegerField(label='Quantity', required=True)
-	OPTIONS_assetType = assettype.makeOptions()
-	assetType = 
-
-	class Meta:
-		model = Asset
-
-
-
-
-
-
-	def __init__(self, *args, **kwargs):
-		if kwargs:
-			self.user = kwargs.pop('currentUser', None)
-		super().__init__(*args, **kwargs)
-
-
-
-
-	def clean(self, *args, **kwargs):
-		if not self.user:
-			return
-		else:
-			assetName = self.cleaned_data.get('assetName')
-			assetQuantity = self.cleaned_data.get('assetQuantity')
-		
-		
-
-	def save(self):
-        form = insert_assets(request.POST)
-        if form.isvalid():
-            form.save()
-        else:
-            form.save()
+#rom django import forms
+#rom .models import Asset
+#mport datetime
+#rom pip._vendor.urllib3 import request
+#rom FormManagement.models import Form
+#
+#
+#lass InsertAssetForm(forms.Form):
+#	user = None
+#	assetName = forms.CharField(label='Asset Name', max_length=255, required=False)
+#	assetQuantity = forms.IntegerField(label='Quantity', required=True)
+#	OPTIONS_assetType = assettype.makeOptions()
+#	assetType = 
+#
+#	class Meta:
+#		model = Asset
+#
+#
+#
+#
+#
+#
+#	def __init__(self, *args, **kwargs):
+#		if kwargs:
+#			self.user = kwargs.pop('currentUser', None)
+#		super().__init__(*args, **kwargs)
+#
+#
+#
+#
+#	def clean(self, *args, **kwargs):
+#		if not self.user:
+#			return
+#		else:
+#			assetName = self.cleaned_data.get('assetName')
+#			assetQuantity = self.cleaned_data.get('assetQuantity')
+#		
+#		
+#
+#	def save(self):
+#       form = insert_assets(request.POST)
+#       if form.isvalid():
+#           form.save()
+#       else:
+#           form.save()
