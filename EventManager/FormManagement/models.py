09ddf0732bb3ff533732e1ea3da12f34a596a4fa
beybladeuser <rubencruz0312@gmail.com>
2021-07-02 18:05:55 +0100

    more changes

--- EventManager/FormManagement/models.py
+++ EventManager/FormManagement/models.py
@@ -127,7 +127,7 @@ class Form(models.Model):
         return (user.id == self.createdby.id or user.groups.filter(pk=1).exists() or self.published) and not user.groups.filter(pk=2).exists()
 
     def canPublish(self, user) :
-        return self.userHasEditPermitions(user) and len(self.formquestions) > 0 and self.isValid() == 0
+        return self.userHasEditPermitions(user) and not self.archived and len(self.formquestions) > 0 and self.isValid() == 0
 
     def canArchive(self, user) :
         return self.userHasEditPermitions(user) and not self.archived and len(self.formquestions) > 0 and self.isValid() == 0
